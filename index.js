2c6a3f05f95d14a08e7d010e67408f16cba14722
David <davidspader97@gmail.com>
2022-08-02 21:08:57 -0300

    wip

---
+++ index.js
@@ -0,0 +1,12 @@
+aphelios = {
+    initial_ammo: 50,
+    weapons: [ 
+        {name: 'Calibrum', ammo: initial_ammo}, 
+        {name: 'Severum', ammo: initial_ammo},
+        {name: 'Gravitum', ammo: initial_ammo},
+        {name: 'Infernum', ammo: initial_ammo},
+        {name: 'Crescendum', ammo: initial_ammo}
+    ]
+}
+
+console.log(aphelios);
